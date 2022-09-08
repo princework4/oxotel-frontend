d54d86aff02615df3bca7eca4c493a0a023300a8
princework4 <princework4@gmail.com>
2022-09-08 14:01:46 +0530

    modified some UI

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 export { default as Logo } from "./Logo/Oxotel.jpg";
+// export { default as Logo } from "./Logo/Oxotel.png";
 export { default as Bed } from "./Logo/bed.jpg";
 export { default as Male } from "./Logo/male.jpg";
 export { default as Female } from "./Logo/female.jpg";
